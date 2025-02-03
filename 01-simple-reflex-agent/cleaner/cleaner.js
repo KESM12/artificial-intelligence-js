4a7c019e2e8f50e2e7781003320b87c80a82a9c5
Kevin Secaida <92343645+KESM12@users.noreply.github.com>
2025-02-03 07:53:54 -0600

    Update cleaner.js

--- 01-simple-reflex-agent/cleaner/cleaner.js
+++ 01-simple-reflex-agent/cleaner/cleaner.js
@@ -5,35 +5,35 @@ function reflex_agent(location, state) {
     if (state == "DIRTY") return "CLEAN";
     else if (location == "A") return "RIGHT";
     else if (location == "B") return "LEFT";
+    else if (location == "C") return "UP";
+    else if (location == "D") return "DOWN";
 }
 
 function test(states, visitedStates) {
     var location = states[0];
-    var state = location == "A" ? states[1] : states[2];
-    var action_result = reflex_agent(location, state);
+    var state = states[1][location];
     
+    visitedStates.add(location + "-" + state);
+    
+    var action_result = reflex_agent(location, state);
     document.getElementById("log").innerHTML += "<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
     
-    // Guardar estado actual
-    var stateKey = states.join();
-    visitedStates.add(stateKey);
-    
-    // Aplicar acción
     if (action_result == "CLEAN") {
-        if (location == "A") states[1] = "CLEAN";
-        else if (location == "B") states[2] = "CLEAN";
-    } else if (action_result == "RIGHT") states[0] = "B";
-    else if (action_result == "LEFT") states[0] = "A";
-    
-    // Verificar si ya visitamos los 8 estados
-    if (visitedStates.size >= 8) {
-        document.getElementById("log").innerHTML += "<br><strong>Se han visitado los 8 estados, finalizando.</strong>";
-        return;
+        states[1][location] = "CLEAN";
+    } else {
+        if (action_result == "RIGHT") states[0] = "B";
+        else if (action_result == "LEFT") states[0] = "A";
+        else if (action_result == "UP") states[0] = "C";
+        else if (action_result == "DOWN") states[0] = "D";
     }
     
-    setTimeout(function () { test(states, visitedStates); }, 1000);
+    if (visitedStates.size < 8) {
+        setTimeout(function() { test(states, visitedStates); }, 2000);
+    } else {
+        document.getElementById("log").innerHTML += "<br>All states visited. Stopping simulation.";
+    }
 }
 
-var states = ["A", "DIRTY", "DIRTY"];
+var states = ["A", {"A": "DIRTY", "B": "DIRTY", "C": "DIRTY", "D": "DIRTY"}];
 var visitedStates = new Set();
 test(states, visitedStates);

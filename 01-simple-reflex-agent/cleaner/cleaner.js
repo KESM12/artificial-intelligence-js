8e48ae33ce7c67aeb2b72f37fd658d382c630023
KESM12 <3001920290101@ingenieria.usac.edu.gt>
2025-03-08 06:46:02 -0600

    Update cleaner.js

--- 01-simple-reflex-agent/cleaner/cleaner.js
+++ 01-simple-reflex-agent/cleaner/cleaner.js
@@ -1,12 +1,12 @@
 // MIT License
 // Copyright (c) 2025 Kevin Secaida
+// MIT License
+// Copyright (c) 2025 Kevin Secaida
 
 function reflex_agent(location, state) {
     if (state == "DIRTY") return "CLEAN";
     else if (location == "A") return "RIGHT";
     else if (location == "B") return "LEFT";
-    else if (location == "C") return "UP";
-    else if (location == "D") return "DOWN";
 }
 
 function test(states, visitedStates) {
@@ -23,12 +23,10 @@ function test(states, visitedStates) {
     } else {
         if (action_result == "RIGHT") states[0] = "B";
         else if (action_result == "LEFT") states[0] = "A";
-        else if (action_result == "UP") states[0] = "C";
-        else if (action_result == "DOWN") states[0] = "D";
     }
     
     if (visitedStates.size < 8) {
-        setTimeout(function() { test(states, visitedStates); }, 2000);
+        setTimeout(function() { test(states, visitedStates); }, 1000);
     } else {
         document.getElementById("log").innerHTML += "<br>All states visited. Stopping simulation.";
     }
@@ -37,3 +35,4 @@ function test(states, visitedStates) {
 var states = ["A", {"A": "DIRTY", "B": "DIRTY", "C": "DIRTY", "D": "DIRTY"}];
 var visitedStates = new Set();
 test(states, visitedStates);
+
